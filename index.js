7a8112bc3c8d4273b8808bc41bcb47022a2d726e
nuzze <mati280341@gmail.com>
2020-09-26 03:39:20 -0300

    [FIX]

--- index.js
+++ index.js
@@ -13,4 +13,6 @@ const port = process.env.PORT || 4000;
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 
-app.listen(port, '0.0.0.0');
+app.listen(port, '0.0.0.0', () => {
+  console.log('server running');
+});
